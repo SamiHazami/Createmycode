f2035e381241d32ae26bd11eb1515afe14484ac1
SamiHazami <sa.hazemi@gmail.com>
2022-06-04 19:20:38 +0100

    update

--- DOM_project/script_DOM.js
+++ DOM_project/script_DOM.js
@@ -1,27 +1,27 @@
 /*************** Setting initial quantities ****************/
 let quantities = Array.from(document.querySelectorAll('.qte'))
 for (let quantity of quantities){
-    quantity.setAttribute('value',1)
+    quantity.setAttribute('value',0)
 }
 /*************** Setting initial prices ****************/
 var price= [];
 var pu=[];
 for (let i=1;i<=5;i++){
-price[i]=25;
+price[i]=0;
 pu[i]=25;
 }
 
 let total_1=price[1]+price[2]+price[3]
-document.querySelector('.price-1').innerHTML=price[1]+' dt'
-document.querySelector('.price-2').innerHTML=price[2]+' dt'
-document.querySelector('.price-3').innerHTML=price[3]+' dt'
-document.querySelector('.total_1').innerHTML=total_1+' dt'
+document.querySelector('.price-1').innerHTML=0+' dt'
+document.querySelector('.price-2').innerHTML=0+' dt'
+document.querySelector('.price-3').innerHTML=0+' dt'
+document.querySelector('.total_1').innerHTML=0+' dt'
 let total_2=price[4]+price[5]
-document.querySelector('.price-4').innerHTML=price[4]+' dt'
-document.querySelector('.price-5').innerHTML=price[5]+' dt'
-document.querySelector('.total_2').innerHTML=total_2 +' dt'
+document.querySelector('.price-4').innerHTML=0+' dt'
+document.querySelector('.price-5').innerHTML=0+' dt'
+document.querySelector('.total_2').innerHTML=0+' dt'
 let total=total_1+total_2
-document.querySelector('.total').innerHTML=total +' dt'
+document.querySelector('.total').innerHTML=0 +' dt'
 
 
 /*************** Total price ****************/
@@ -60,7 +60,7 @@ for(let i=4;i<=5;i++){
 for (let i=1;i<=3;i++){
   document.querySelector(`.item-${i} > .minus-btn`)
 .addEventListener("click", function(e) {
-    if(Number(document.querySelector(`.qte-${i}`).getAttribute('value')) >1){
+    if(Number(document.querySelector(`.qte-${i}`).getAttribute('value')) >0){
         document.querySelector(`.qte-${i}`).setAttribute('value',
         Number(document.querySelector(`.qte-${i}`).getAttribute('value'))-1);
         price[i]=price[i]-pu[i];
@@ -78,7 +78,7 @@ for (let i=1;i<=3;i++){
 for (let i=4;i<=5;i++){
     document.querySelector(`.item-${i} > .minus-btn`)
   .addEventListener("click", function(e) {
-      if(Number(document.querySelector(`.qte-${i}`).getAttribute('value')) >1){
+      if(Number(document.querySelector(`.qte-${i}`).getAttribute('value')) >0){
           document.querySelector(`.qte-${i}`).setAttribute('value',
           Number(document.querySelector(`.qte-${i}`).getAttribute('value'))-1);
           price[i]=price[i]-pu[i];
